e36cc8874a2f3f283f45923012fe4f5f6f71e254
sakg-dev <sakg.dev.14@gmail.com>
2026-05-26 14:25:30 +0530

    some pynput tests

--- experiements/_pynput.py
+++ experiements/_pynput.py
@@ -1,8 +1,9 @@
-from pynput.keyboard import Key, Controller as kController
+# from pynput.keyboard import Key, Controller as kController
 from pynput.mouse import Button, Controller as mController
 from time import sleep
+import random
 
-keyboard = kController()
+# keyboard = kController()
 mouse = mController()
 
 # while True:
@@ -15,14 +16,45 @@ mouse = mController()
 #     keyboard.release(Key.shift)
 #     sleep(5)
 
+# sleep(5)
+# while True:
+#     sleep(1) # 90 cps lol
+#     mouse.release(Button.left)
+#     mouse.release(Button.right)
+#     keyboard.release("W")
+#     keyboard.release("A")
+#     keyboard.release("S")
+#     keyboard.release("D")
+#     keyboard.release(Key.shift)
+#     keyboard.release(Key.space)
+
+
 sleep(5)
+info = {
+    "time": 0,
+    "for": "left"
+}
+for_list = ["left", "right", "top", "bottom"]
+val = 5
 while True:
-    sleep(1) # 90 cps lol
-    mouse.release(Button.left)
-    mouse.release(Button.right)
-    keyboard.release("W")
-    keyboard.release("A")
-    keyboard.release("S")
-    keyboard.release("D")
-    keyboard.release(Key.shift)
-    keyboard.release(Key.space)
+    if info["time"] < 300:
+        for_val = info["for"]
+        x = 0
+        y = 0
+        if for_val == "left":
+            x = -val
+        elif for_val == "right":
+            x = val
+        if for_val == "top":
+            y = -val
+        elif for_val == "bottom":
+            y = val
+        mouse.move(x, y)
+        sleep(0.01)
+        info["time"] += 1
+    else:
+        new_move = for_list[random.randint(0, len(for_list)-1)]
+        info = {
+            "time": 0,
+            "for": new_move
+        }
